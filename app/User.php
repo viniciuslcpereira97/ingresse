1beab808aa9685465ae037d0fda72900501c5e51
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-19 12:22:19 -0300

    company relationship

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace Ingresse;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Ingresse\Company;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -26,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     *
+     * Company relationship
+     *
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
