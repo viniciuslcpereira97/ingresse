d58b6ab9605973fae635a0d81436ad5f739feb2f
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-19 11:32:28 -0300

    interface methods implemented

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,5 +6,30 @@ use Ingresse\Contracts\Repository;
 
 class UserRepository implements Repository
 {
-    
+
+    public function all()
+    {
+        return 'all';
+    }
+
+    public function store($data)
+    {
+        dd($data);
+    }
+
+    public function get(int $id)
+    {
+        dd($id);
+    }
+
+    public function update($data, int $id)
+    {
+        dd($data);
+    }
+
+    public function delete(int $id)
+    {
+        dd($id);
+    }
+
 }
