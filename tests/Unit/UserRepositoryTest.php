40795f594a896194722058a042b5a6ff149f15c4
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-20 10:02:39 -0300

    covering all repository methods

--- tests/Unit/UserRepositoryTest.php
+++ tests/Unit/UserRepositoryTest.php
@@ -17,20 +17,54 @@ class UserRepositoryTest extends TestCase
     {
         parent::setUp();
         $this->repository = new UserRepository;
+        $this->user = factory(\Ingresse\User::class)->make();
     }
 
+    // All users was stored and retrieved
     public function testGetAllUsers()
     {
+        $fakeUsers = factory(\Ingresse\User::class, 5)->create();
         $users = $this->repository->all();
 
-        $this->assertEquals($users->count(), 0);
+        $this->assertEquals($users->count(), $fakeUsers->count());
     }
 
+    // Is retrieving user
+    public function testGetUser()
+    {
+        $user = $this->repository->store($this->user->getAttributes());
+
+        $this->assertEquals(
+            $this->repository->get($user->id)->email,
+            $this->user->email
+        );
+    }
+
+    // User was stored
     public function testInsertUser()
     {
-        $user = factory(\Ingresse\User::class)->make();
-        $this->repository->store($user->getAttributes());
+        $this->repository->store($this->user->getAttributes());
 
-        $this->assertDatabaseHas('users', ['email' => $user->email]);
+        $this->assertDatabaseHas('users', ['email' => $this->user->email]);
     }
+
+    // User was deleted
+    public function testDeleteUser()
+    {
+        $user = $this->repository->store($this->user->getAttributes());
+        $this->repository->delete($user->id);
+
+        $this->assertDatabaseMissing('users', ['email' => $this->user->email]);
+    }
+
+    // User was updated
+    public function testUpdateUser()
+    {
+        $user = $this->repository->store($this->user->getAttributes());
+        $newEmail = factory(\Ingresse\User::class)->make()->email;
+        $this->repository->update(['email' => $newEmail], $user->id);
+
+        $this->assertDatabaseMissing('users', ['email' => $this->user->email]);
+    }
+
 }
