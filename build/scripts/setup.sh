6c00b2e5e85f11c8e1cb0ce628039cee08704faf
Vinicius Luiz <vinicius.luiz@ingresse.com>
2019-01-09 17:10:06 -0200

    add setup script

---
+++ build/scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo service docker start
+
+sudo docker-compose -f ../docker-compose.yml up -d
+sudo docker-compose -f ../docker-compose.yml exec app chown -R www-data ../storage
+sudo docker-compose -f ../docker-compose.yml exec app chmod -R 775 ../storage
+sudo docker-compose -f ../docker-compose.yml exec app composer install
