5457edb9965fd2b54dde82ad632c3b9bc508d70b
Vinicius Luiz <vinicius.luiz@ingresse.com>
2019-01-09 17:13:22 -0200

    update setup script

--- build/scripts/setup.sh
+++ build/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 sudo service docker start
 
-sudo docker-compose -f ../docker-compose.yml up -d
-sudo docker-compose -f ../docker-compose.yml exec app chown -R www-data ../storage
-sudo docker-compose -f ../docker-compose.yml exec app chmod -R 775 ../storage
-sudo docker-compose -f ../docker-compose.yml exec app composer install
+sudo docker-compose -f ../../docker-compose.yml up -d --force-recreate
+sudo docker-compose -f ../../docker-compose.yml exec app chown -R www-data storage
+sudo docker-compose -f ../../docker-compose.yml exec app chmod -R 775 storage
+sudo docker-compose -f ../../docker-compose.yml exec app composer install
